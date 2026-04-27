5165dbfd12c50369792cf5a20c7823df5d35e5a3
Ryan Oldenburg <ryan@guzba.com>
2026-04-27 15:27:43 -0500

    noise for github

--- src/mummy.nim
+++ src/mummy.nim
@@ -11,8 +11,6 @@ import mummy/common, mummy/internal, std/atomics, std/base64,
     std/tables, std/times, webby/httpheaders, webby/queryparams, webby/urls,
     zippy, std/options
 
-noise for github
-
 from std/strutils import find, cmpIgnoreCase, toLowerAscii
 
 when defined(linux):
